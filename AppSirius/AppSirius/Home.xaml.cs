7144cf6cc2deee23beab870adeb92a193fdbbfe5
Jabberwocky <Jabberwocky@DESKTOP-193U9G8>
2022-12-14 11:37:21 -0300

    Add project files.

---
+++ AppSirius/AppSirius/Home.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSirius
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        async void OnTapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Confirmation", "Seizure registered", "OK");
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
